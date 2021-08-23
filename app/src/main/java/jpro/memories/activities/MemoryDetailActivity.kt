a4af6faaaa8c61df012a80a6db77cc2b95b5fbbe
Jana Protic <proticjana@gmail.com>
2021-08-23 12:40:52 +0200

    Remove leftovers from image idea

---
+++ app/src/main/java/jpro/memories/activities/MemoryDetailActivity.kt
@@ -0,0 +1,12 @@
+package jpro.memories.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import jpro.memories.R
+
+class MemoryDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_memory_detail)
+    }
+}
